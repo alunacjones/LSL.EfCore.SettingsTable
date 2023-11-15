075cc8e1e4116b824dd8ccc2557d0f9fcf88f5f1
Alun Jones <alunacjones@gmail.com>
2023-11-15 23:41:14 +0000

    entity exlucde from ocov

--- LSL.EfCore.SettingsTable/Entities/Setting.cs
+++ LSL.EfCore.SettingsTable/Entities/Setting.cs
@@ -1,11 +1,13 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace LSL.EfCore.SettingsTable.Entities
 {
     /// <summary>
     /// A Key/Value Setting
     /// </summary>
+    [ExcludeFromCodeCoverage]
     public class Setting
     {
         /// <summary>
